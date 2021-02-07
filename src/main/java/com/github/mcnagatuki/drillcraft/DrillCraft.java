03cf4ae3c04d50f4c1345c2e11e7b6ba012bd174
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2021-02-07 16:25:50 +0900

    first commit

---
+++ src/main/java/com/github/mcnagatuki/drillcraft/DrillCraft.java
@@ -0,0 +1,17 @@
+package com.github.mcnagatuki.drillcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DrillCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
